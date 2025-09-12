3c967c39762ce4e70f92cb352e1f9e7ddce5640f
Animesh Anand <animeshanand86033@gmail.com>
2025-09-12 16:55:07 +0530

    pointerstarted

---
+++ poiners.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<< sizeof(int) <<" bytes" <<endl;
+    cout<< sizeof(float) <<" bytes" <<endl;
+    cout<< sizeof(bool) <<" bytes" <<endl;
+    cout<< sizeof(long long) <<" bytes" <<endl;
+    cout<< sizeof(double) <<" bytes" <<endl;
+    cout<< sizeof(int) <<" bytes" <<endl;
+
+
+
+    int x = 4;
+    int* p =&x;
+
+    cout<< &x;
+}
