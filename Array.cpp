12caf5efb865b2e3cc3b7631799d2d36f0353286
Animesh Anand <animeshanand86033@gmail.com>
2025-09-20 17:33:17 +0530

    sum

--- Array.cpp
+++ Array.cpp
@@ -46,16 +46,28 @@ int main(){
         // }
         //finding size
 
-        int arr[] = {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1};
+        // int arr[] = {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1};
 
-        int Size = sizeof(arr) / sizeof(arr[0]); // b'z 1 int = 4 byte so full array ka size divided by 1 element ka size
-        cout<<"Size of Array: " <<Size;
+        // int Size = sizeof(arr) / sizeof(arr[0]); // b'z 1 int = 4 byte so full array ka size divided by 1 element ka size
+        // cout<<"Size of Array: " <<Size;
 
     
 
-
-
-
+//Ques : Calculate the sum of all the elements in the given array.
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Elements af array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum = sum + arr[i];
+    }
+    cout<<"Sum = "<< sum;
+    
 
 
 }
