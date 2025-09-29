b9d63c21d37c169122441376f699398d3eb2135b
Animesh Anand <animeshanand86033@gmail.com>
2025-09-29 23:30:12 +0530

    vloo

--- Vector.cpp
+++ Vector.cpp
@@ -8,7 +8,29 @@ int main(){
     v.push_back(3);
     v.push_back(4);
     v.push_back(5);
-    v.push_back(6);
 
-    cout<<v[0];
+    v[4] = 9; //updating the value
+     
+
+    // cout<<v[0]<<" ";
+    // cout<<v[1]<<" ";
+    // cout<<v[2]<<" ";
+    // cout<<v[3]<<" ";
+    // cout<<v[4]<<" ";
+
+    
+    for(int i=0;i<v.size();i++){ //we can print value with loop as well
+        cout<<v[i] << " ";
+    }
+
+    cout<<"\nCurrent size = " << v.size();
+    cout<<"\nCurrent capacity = " << v.capacity();
+
+    v.pop_back(); // removing last element
+    
+
+    // Size = how many elements are in the vector right now.
+
+    // Capacity = how many elements the vector can hold without allocating more memory.
+    
 }
