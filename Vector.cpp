3e8f40d8764e6c6bd4b1977347682d370646e9c5
Animesh Anand <animeshanand86033@gmail.com>
2025-10-06 13:45:24 +0530

    passingVector to a function

--- Vector.cpp
+++ Vector.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+// int main(){
     // vector <int> v;
     // v.push_back(1);
     // v.push_back(2);
@@ -67,25 +67,49 @@ int main(){
 
     //At property
 
+    // vector <int> v;
+    // v.push_back(4);
+    // v.push_back(9);
+    // v.push_back(1);
+    // v.push_back(7);
+
+
+    // for (int i=0;i<v.size();i++){
+    //     cout<<v.at(i)<<" ";
+    // }
+    // cout<<endl;
+
+    // sort(v.begin(), v.end());
+    // cout<<"Shorted Array: ";
+    // for (int i=0;i<v.size();i++){
+    //     cout<<v.at(i)<<" ";
+    // }
+    // cout<<endl;
+
+     
+// }
+
+void change(vector<int>&a){ //& ->ampercent oprator
+    a[0]=100;
+}
+int main(){
     vector <int> v;
     v.push_back(4);
     v.push_back(9);
     v.push_back(1);
     v.push_back(7);
-
-
-    for (int i=0;i<v.size();i++){
-        cout<<v.at(i)<<" ";
-    }
-    cout<<endl;
-
-    sort(v.begin(), v.end());
-    cout<<"Shorted Array: ";
-    for (int i=0;i<v.size();i++){
-        cout<<v.at(i)<<" ";
-    }
-    cout<<endl;
-
-
     
+    for (int i=0;i<v.size();i++){
+        cout<<v.at(i)<<" ";
+    }
+    cout<<endl;
+
+
+    change(v); //jab refrence tabhi change hoga nahi to wo naya array banata hai
+
+    for (int i=0;i<v.size();i++){
+        cout<<v.at(i)<<" ";
+    }
+    cout<<endl;
+
 }
