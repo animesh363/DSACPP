b538c102363d5eaa1b76ca54f81369a0056fd42e
Animesh Anand <animeshanand86033@gmail.com>
2025-12-27 00:42:44 +0530

    starting with strings

---
+++ Strings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char str[] = {'A','n','i','m','s','h'};
+
+    // for (int i=0;i<6;i++){
+    //     cout<<str[i] <<" ";
+    // }
+
+    char ch= 'a';
+    cout<<int(ch);
+}
