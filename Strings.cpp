951ddf546bf3681ae70a030c68320ac4678b766c
Animesh Anand <animeshanand86033@gmail.com>
2025-12-28 00:19:39 +0530

    done with character array and started with string

--- Strings.cpp
+++ Strings.cpp
@@ -2,12 +2,44 @@
 using namespace std;
 
 int main(){
-    char str[] = {'A','n','i','m','s','h'};
+    //?             __________Character Array__________             
+    // char str[] = {'A','n','i','m','s','h'};
 
-    // for (int i=0;i<6;i++){
-    //     cout<<str[i] <<" ";
+    // // for (int i=0;i<6;i++){
+    // //     cout<<str[i] <<" ";
+    // // }
+
+    // char ch= '\0';//0
+    // cout<<int(ch);
+
+    // char str1[]={'a','b','c','d','\0'}; //-->\0 used for terminating the it is added by default
+
+    // char str2[]={'a','b','\0','c','d'};
+    // for(int i=0;str2[i]!='\0';i++){
+    //     cout<<str2[i]<<" ";
     // }
 
-    char ch= 'a';
-    cout<<int(ch);
+
+
+    //?             __________Strings__________ 
+
+    // string str1 ="Animesh Anand"; //"Animesh Anand" -->This is treated as 14 size of char array 13 with space and 14 with \0 at the last
+    // cout<<str1;
+
+    //Name directly as a input
+
+    // string name;
+    // cout<<"Enter Youre name: ";
+    // cin>>name;
+    // cout<< "Your name is "<< name;
+
+    //Problem
+    // string str;
+    // cin>>str;//Animesh Anand -> Animesh after space it is not taking it as a input
+    // cout<<str;
+
+    //solution 
+    string str2;
+    getline(cin,str2);
+    cout<<str2;
 }
