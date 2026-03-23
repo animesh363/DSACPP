5c36a3b38afe512302a97b59fe8da5c0a20e50db
animesh363 <animeshanand86033@gmail.com>
2026-03-23 17:04:52 +0530

    bubbbloptimised

--- BubbleSort/bsorting2.cpp
+++ BubbleSort/bsorting2.cpp
@@ -11,14 +11,41 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-    for(int i=0;i<n-1;i++){ //n-1 passes
+    // for(int i=0;i<n-1;i++){ //n-1 passes
+    //     //travesing
+    //     for(int j=0;j<n-1;j++){
+    //         if(arr[j]>arr[j+1]){
+    //             swap(arr[j],arr[j+1]);
+    //         }
+    //     }
+    // }
+
+
+    // for(int i=0;i<n-1;i++){ //Best lass complexity
+    //     //travesing
+    //     for(int j=0;j<n-1-i;j++){
+    //         if(arr[j]>arr[j+1]){
+    //             swap(arr[j],arr[j+1]);
+    //         }
+    //     }
+    // }
+
+    //optimised method
+
+    for(int i=0;i<n-1;i++){ 
+        bool swapped = false;
         //travesing
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n-1-i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped = true;            
             }
         }
+        if(swapped == false){
+            break;
+        }
     }
+
     cout<<endl <<"After Sorting "<<endl;
     for(int i=0;i<n;i++){
         cout<<arr[i] <<" ";
