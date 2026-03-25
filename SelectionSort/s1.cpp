61616bcb8f7c17042bf8a9d36df5a1b9eab1fe16
animesh363 <animeshanand86033@gmail.com>
2026-03-26 02:55:52 +0530

    selection sort

---
+++ SelectionSort/s1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5;
+    int arr[n]={5,3,1,4,2};
+
+    cout<<"Element before sorting"<<endl;
+    for(int ele : arr){
+        cout<<ele<<" ";
+    }
+
+    int min;
+    for(int i=0;i<n-1;i++){
+        int min=INT_MAX;
+        int mindx=-1;
+
+        for(int j=i;j<n;j++){
+            if(arr[j]<=min){
+                min = arr[j];
+                mindx = j;
+
+            }
+        }
+        swap(arr[i],arr[mindx]);
+    }
+
+    cout<<"\nAfter Selection sort: "<<endl;
+    for(int ele : arr){ //this is is foreach loop eg-> for every element in arr cout element
+        cout<<ele<<" ";
+    }
+
+
+}
