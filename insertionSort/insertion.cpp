edd36363dcb47d677474a31c752af55624447010
animesh363 <animeshanand86033@gmail.com>
2026-03-27 05:43:40 +0530

    Insertion sort

---
+++ insertionSort/insertion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5;
+    int arr[n]={5,3,1,4,2};
+
+    cout<<"Element before sorting"<<endl;
+    for(int i=0;i<n;i++){ 
+        cout<<arr[i]<<" ";
+    }
+
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>=1&& arr[j]<arr[j-1]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+    
+
+    cout<<"\nAfter Insertion sort: "<<endl;
+    for(int i=0;i<n;i++){ 
+        cout<<arr[i]<<" ";
+    }
+
+
+}
