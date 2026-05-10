fc33248c28b71844aa6de749034b2c2292d8cd1a
animesh363 <animeshanand86033@gmail.com>
2026-05-11 01:02:32 +0530

    maxArrRec

---
+++ Recursion/maxArrREc.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printMax(int arr[],int n,int idx,int Max){
+    if(idx==n){
+        cout<<Max;
+        return;
+    } 
+    if(arr[idx]>Max) Max = arr[idx];
+    printMax(arr,n,idx+1,Max);
+
+
+}
+int main(){
+    int arr[] = {2,5,3,6,6,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Max element: ";
+    printMax(arr,n,0,INT_MIN);
+    
+}
