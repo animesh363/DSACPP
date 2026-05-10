e0f1196ffa4df761ba1b6b5a06e1b6fd781f3ea9
animesh363 <animeshanand86033@gmail.com>
2026-05-10 15:56:31 +0530

    towr of hanoi added

---
+++ Recursion/practice/combination.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int steps=0;
+void hanoi(int n, char a, char b, char c){
+    if(n==0) return;
+    hanoi(n-1,a,c,b);
+    cout<< a <<" -> "<< c << endl;
+    steps++;
+    hanoi(n-1,b,a,c);
+}
+int main(){
+    int n; cout<<"Enter n: "; cin>>n;
+    hanoi(n,'A','B','C');
+    cout<<"Total no of steps: " <<steps<<endl;
+}
