4d891f2a0c511adea8a714e02e67129d946e6303
Animesh Thakur <animeshanand86033@gmail.com>
2026-07-05 17:23:43 +0530

    first from fedora

---
+++ next.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // pair<int, int> p = {2,4};
+    // cout<<p.first <<" " <<p.second;
+    
+    //nested pairs
+    
+    // pair<int, pair<int, int>>p = {1,{2,3}};
+    // cout<<p.first <<" "<<p.second.first <<" " <<p.second.second;,
+    
+    //array contaning pairs
+    
+    pair<int,int> arr[] = {{1,2}, {3,4}, {5,6}};
+    cout<<arr[1].first  << " " << arr[1].second;
+}
