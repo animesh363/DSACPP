faf671fff14e1cdffaad57f6531c546f9849c80a
Animesh Thakur <animeshanand86033@gmail.com>
2026-09-02 14:48:14 +0530

    Supirior added

---
+++ accenture/superior.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSup(int arr[],int n){
+    int cnt=1;
+    int supMax=arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        if(arr[i]>supMax){
+            cnt++;
+            supMax=arr[i];
+        }
+    }
+    return cnt;
+}
+int main(){
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result = findSup(arr,n);
+    cout<<result;
+}
